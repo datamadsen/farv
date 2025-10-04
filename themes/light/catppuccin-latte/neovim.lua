761a07fb69f0408d89d41c040c7a9aece4745ecd
Tim M. Madsen <tim@datamadsen.dk>
2025-10-04 14:43:43 +0200

    Catpuccin latte

---
+++ themes/light/catppuccin-latte/neovim.lua
@@ -0,0 +1,20 @@
+-- Neovim theme configuration for catppuccin-latte
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "latte",
+			})
+			vim.cmd.colorscheme("catppuccin-latte")
+		end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-latte",
+		},
+	},
+}
