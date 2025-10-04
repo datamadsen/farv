be6ab327dc9535187d2895279c32171fece8b66c
Tim M. Madsen <tim@datamadsen.dk>
2025-10-04 14:37:34 +0200

    Fix but in how tmux  was applied

--- themes/tmux.sh
+++ themes/tmux.sh
@@ -2,8 +2,9 @@
 
 source /usr/share/farv/lib/utils.sh
 
-if ! has_command tmux || ! is_running tmux; then
+if ! has_command tmux || ! tmux has-session 2>/dev/null; then
+  echo "tmux is not running"
   exit 0
 fi
 
-tmux source-file ~/.tmux.conf 2>/dev/null
+tmux source-file ~/.tmux.conf
